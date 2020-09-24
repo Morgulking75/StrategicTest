06a4b744870ef7adb9a07467c6f778f66e0839b9
Karl Hoglander <k.hoglander@gmail.com>
2020-09-24 14:02:08 -0400

    getting the data to show up, adding in the right columns, summarizing across three fields adding remove debt button with functionality adding add debt button without functionality adding in checkbox functionality

--- StrategicTest/src/app/app.component.ts
+++ StrategicTest/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StrategicData } from './StrategicData';
 
 @Component({
 	selector: 'app-root',
@@ -7,6 +8,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'StrategicTest';
+	data: StrategicData[];
+	total: number;
+	totalRowCount: number;
+	totalCheckRowCount: number;
+	toggleChecked: boolean;
 
+	async getStrategicData(): Promise<StrategicData[]> {
+		const data = await fetch('https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json');
+		const jsonData = await data.json();
 
+		this.data = (jsonData as StrategicData[]);
+		this.updateCheckCount();
+		this.updateTotalRowCount();
+		this.updateTotalBalance();
+
+		return this.data;
+	}
+
+	ngOnInit() {
+		this.getStrategicData();
+	}
+
+	updateCheckCount() {
+		this.totalCheckRowCount = this.data.map(d => d.isChecked).reduce((a, b) => a + (b ? 1 : 0), 0);
+
+		this.toggleChecked = this.totalCheckRowCount === this.totalRowCount;
+	}
+
+	updateTotalRowCount() {
+		this.totalRowCount = this.data.length;
+	}
+
+	updateTotalBalance() {
+		this.total = this.data.map(d => d.balance).reduce((a, b) => a + b, 0);
+	}
+
+	removeRows() {
+		this.data = this.data.filter(d => !d.isChecked);
+		this.updateCheckCount();
+		this.updateTotalRowCount();
+		this.updateTotalBalance();
+	}
+
+	toggleAll() {
+		this.toggleChecked = !this.toggleChecked;
+		this.data.forEach(d => d.isChecked = this.toggleChecked);
+
+		this.updateCheckCount();
+	}
 }
